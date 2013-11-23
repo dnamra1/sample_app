4ff331ffb07491da04700387b80783b316fa06a2
armand <armand.dossantos@s-money.net>
2013-11-23 22:43:21 +0100

    help page created

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,40 +3,55 @@ require 'spec_helper'
 describe PagesController do
 	render_views
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
+	before(:each) do
+		@base_title = "Ruby on Rails sample app | "
+	end
 	
-	it "should have the right title" do
-		get 'home'
-		response.should have_selector("title", :content => "Ruby on Rails sample app | Home")
-	end
-  end
-
-  describe "GET 'contact'" do
-    it "returns http success" do
-      get 'contact'
-      response.should be_success
-    end
+	describe "GET 'home'" do
+		it "returns http success" do
+			get 'home'
+			response.should be_success
+		end
 	
-	it "should have the right title" do
-		get 'contact'
-		response.should have_selector("title", :content => "Ruby on Rails sample app | Contact")
+		it "should have the right title" do
+			get 'home'
+			response.should have_selector("title", :content => @base_title + "Home")
+		end
 	end
-  end
 
-  describe "GET 'about'" do
-	it "returns http success" do
-		get 'about'
-		response.should be_success
+	describe "GET 'contact'" do
+		it "returns http success" do
+			get 'contact'
+			response.should be_success
+		end
+	
+		it "should have the right title" do
+			get 'contact'
+			response.should have_selector("title", :content => @base_title + "Contact")
+		end
 	end
-  end
+
+	describe "GET 'about'" do
+		it "returns http success" do
+			get 'about'
+			response.should be_success
+		end
   
-  	it "should have the right title" do
-		get 'about'
-		response.should have_selector("title", :content => "Ruby on Rails sample app | About")
+		it "should have the right title" do
+			get 'about'
+			response.should have_selector("title", :content => @base_title + "About")
+		end
 	end
-
+	
+	describe "GET 'help'" do
+		it "returns http success" do
+			get 'help'
+			response.should be_success
+		end
+	
+		it "should have the right title" do
+			get 'help'
+			response.should have_selector("title", :content => "Ruby on Rails sample app | Help")
+		end
+	end
 end
